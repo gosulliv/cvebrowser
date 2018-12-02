f129450b2caf3069db439abd00e6db084477f568
Galen O'Sullivan <galen.osullivan@gmail.com>
2018-12-02 17:55:02 -0500

    And it works with UTF-8

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 extern crate rusqlite;
 
-use rusqlite::types::ToSql;
 use rusqlite::{Connection, NO_PARAMS};
 
 #[derive(Debug)]
@@ -15,7 +14,7 @@ struct CVE {
 }
 
 fn main() {
-    let conn = Connection::open_in_memory().unwrap();
+    let conn = Connection::open("cve/cves").unwrap();
 
     let mut stmt = conn
         .prepare("SELECT * FROM CVE")
@@ -23,16 +22,24 @@ fn main() {
 
     let cve_iter = stmt
         .query_map(NO_PARAMS, |row| CVE {
-            name: row.get(0),
-            status: row.get(1),
-            description: row.get(2),
-            refs: row.get(3),
-            phase: row.get(4),
-            votes: row.get(5),
-            comments: row.get(6),
+            name: row.get( 0),
+            status: row.get( 1),
+            description: row.get( 2),
+            refs: row.get( 3),
+            phase: row.get( 4),
+            votes: row.get( 5),
+            comments: row.get( 6),
         }).unwrap();
 
+
+    let mut counter = 0;
+
     for cve in cve_iter {
-        println!("Found cve {:?}", cve.unwrap());
+        //if counter > 100 { return; }
+        match cve {
+            Ok(x) => println!("Found cve #{} {:?}", counter, x),
+            Err(x) => println!("CVE {} has issues, error {:?}", counter, x),
+        }
+        counter += 1;
     }
 }
